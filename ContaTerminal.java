9a4d3af79cf3b14f6c413d5840e4ecdba362170a
Vinicius Tavares Assis <92310141+viniciustassis@users.noreply.github.com>
2024-09-30 11:49:54 -0300

    Create ContaTerminal.java

---
+++ ContaTerminal.java
@@ -0,0 +1,29 @@
+import java.util.Scanner;
+
+public class ContaTerminal {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        
+        // Solicitando e recebendo os dados do usuário
+        System.out.println("Por favor, digite o número da Conta:");
+        int numero = scanner.nextInt();
+        
+        System.out.println("Por favor, digite o número da Agência:");
+        String agencia = scanner.next();
+        
+        // Limpando a linha para evitar problemas com o scanner após nextInt()
+        scanner.nextLine();
+        
+        System.out.println("Por favor, digite o nome do Cliente:");
+        String nomeCliente = scanner.nextLine();
+        
+        System.out.println("Por favor, digite o saldo:");
+        double saldo = scanner.nextDouble();
+        
+        // Exibindo a mensagem final com os dados fornecidos
+        System.out.println("Olá " + nomeCliente + ", obrigado por criar uma conta em nosso banco, " +
+                "sua agência é " + agencia + ", conta " + numero + " e seu saldo " + saldo + " já está disponível para saque.");
+        
+        scanner.close();
+    }
+}
